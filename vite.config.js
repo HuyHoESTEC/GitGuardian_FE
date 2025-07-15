fc6912c2b0e0f91795523facacd9f60d137186d4
HuyHoEstec <huy.ho@biendongco.vn>
2025-07-15 10:31:52 +0700

    Build initialize structure

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',        // Cho phép truy cập từ máy khác
+    port: 5179,             // Chọn port mong muốn
+    strictPort: true,       // Không tự đổi nếu port đang bị chiếm
+  }
 })
